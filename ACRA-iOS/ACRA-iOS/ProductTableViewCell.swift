6732f8f220716958afe159d9694e89ce99e97378
Ankit Luthra <ankitluthra111@gmail.com>
2017-02-16 04:54:37 -0500

    Added 1. Table View cell 2. Have not so working dummy data in the product page 3. Added some dummy images to app for testing

---
+++ ACRA-iOS/ACRA-iOS/ProductTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  ProductTableViewCell.swift
+//  ACRA-iOS
+//
+//  Created by Team Amazon on 2/16/17.
+//  Copyright © 2017 Team Amazon. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ProductTableViewCell: UITableViewCell {
+    //MARK: Properties
+    @IBOutlet weak var photo: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var price: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+}
--- ACRA-iOS/ACRA-iOS/ProductViewController.swift
+++ ACRA-iOS/ACRA-iOS/ProductViewController.swift
@@ -9,7 +9,14 @@
 import Foundation
 import UIKit
 
-class ProductViewController: UIViewController {
+class ProductViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    //MARK: Properties
+    @IBOutlet weak var tableView: UITableView!
+    
+    var names = ["Samsung QN55Q7F Flat 55-Inch 4K Ultra HD Smart QLED TV (2017 Model)", "Samsung QN55Q8C Curved 55-Inch 4K Ultra HD Smart QLED TV (2017 Model)", "Samsung UN65KS8000 65-Inch 4K Ultra HD Smart LED TV (2016 Model)", "Samsung UN40J6200 40-Inch 1080p Smart LED TV (2015 Model)"]
+    var prices = ["$2499.99", "$3499.99", "$1597.99", "$347.99"]
+    //var images = [UIImage(named: "Samsung1"), UIImage(named: "Samsung2"), UIImage(named: "Samsung3"), UIImage(named: "Samsung4")]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,5 +26,19 @@ class ProductViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = self.tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! ProductTableViewCell
+        
+        //cell.photo.image = images[indexPath.row]
+        cell.name.text = names[indexPath.row]
+        cell.price.text = prices[indexPath.row]
+        
+        return cell
+    }
+    
 }
