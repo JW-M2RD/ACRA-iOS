8092488c82a8fa822a3b6d910c0f1b22eefd30f9
Ankit Luthra <ankitluthra111@gmail.com>
2017-03-20 22:36:59 -0400

    Sort in reviewList is ready!!

--- ACRA-iOS/ACRA-iOS/ReviewListTable.swift
+++ ACRA-iOS/ACRA-iOS/ReviewListTable.swift
@@ -12,11 +12,12 @@ import UIKit
 class ReviewListTable: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     //MARK: Properties
-    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var reviewListTableView: UITableView!
     @IBOutlet weak var navigationTitle: UINavigationItem!
     @IBOutlet weak var positiveSegmentedController: UISegmentedControl!
-    @IBAction func SegmentedAction(_ sender: Any) {
-        self.tableView.reloadData()
+    @IBOutlet weak var listSortTableView: UITableView!
+       @IBAction func SegmentedAction(_ sender: Any) {
+        self.reviewListTableView.reloadData()
     }
 
     
@@ -25,6 +26,7 @@ class ReviewListTable: UIViewController, UITableViewDataSource, UITableViewDeleg
     var reviews = Reviews()
     var selectedCategory = String()
     var selectedProductTitle = String ()
+    var sortByTitles = ["First","Second"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,7 +35,7 @@ class ReviewListTable: UIViewController, UITableViewDataSource, UITableViewDeleg
         // Set the prompt(text above title) in navigation bar
         self.navigationItem.prompt = selectedProductTitle.substring(to: selectedProductTitle.index(selectedProductTitle.startIndex, offsetBy: CoreDataHelper.setOffSet(titleCount: selectedProductTitle.characters.count)))
 
-        tableView.tableFooterView = UIView()
+        reviewListTableView.tableFooterView = UIView()
         
         
     }
@@ -44,97 +46,109 @@ class ReviewListTable: UIViewController, UITableViewDataSource, UITableViewDeleg
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
-        
-        if self.selectedCategory == "Product Quality" {
+ 
+        if(tableView == reviewListTableView){
+            if self.selectedCategory == "Product Quality" {
 
-            tableView.backgroundView = nil
+                tableView.backgroundView = nil
             
-            switch self.positiveSegmentedController.selectedSegmentIndex {
-            case 0:
-                if(self.reviews.rePosReviews.count == 0){
-                    setDogImg()
-                    return 0
+                switch self.positiveSegmentedController.selectedSegmentIndex {
+                case 0:
+                    if(self.reviews.rePosReviews.count == 0){
+                        setDogImg()
+                        return 0
+                    }
+                    return self.reviews.rePosReviews.count
+                case 1:
+                    if(self.reviews.reNegReviews.count == 0){
+                        setDogImg()
+                        return 0
+                    }
+                    return self.reviews.reNegReviews.count
+                default:
+                    break
                 }
-                return self.reviews.rePosReviews.count
-            case 1:
-                if(self.reviews.reNegReviews.count == 0){
-                    setDogImg()
-                    return 0
-                }
-                return self.reviews.reNegReviews.count
-            default:
-                break
             }
+            else {
+                switch self.positiveSegmentedController.selectedSegmentIndex {
+                case 0:
+                    if(self.reviews.irPosReviews.count == 0){
+                        setDogImg()
+                        return 0
+                    }
+                    return self.reviews.irPosReviews.count
+                case 1:
+                    if(self.reviews.irNegReviews.count == 0){
+                        setDogImg()
+                        return 0
+                    }
+                    return self.reviews.irNegReviews.count
+                default:
+                    break
+                }
+            }
+        
+            return 0
         }
         else {
-            switch self.positiveSegmentedController.selectedSegmentIndex {
-            case 0:
-                if(self.reviews.irPosReviews.count == 0){
-                    setDogImg()
-                    return 0
-                }
-                return self.reviews.irPosReviews.count
-            case 1:
-                if(self.reviews.irNegReviews.count == 0){
-                    setDogImg()
-                    return 0
-                }
-                return self.reviews.irNegReviews.count
-            default:
-                break
-            }
+            return self.sortByTitles.count
         }
-        
-        return 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = self.tableView.dequeueReusableCell(withIdentifier: "ReviewCell", for: indexPath) as! ReviewListCell
+        if(tableView == reviewListTableView){
+
+            let cell = self.reviewListTableView.dequeueReusableCell(withIdentifier: "ReviewCell", for: indexPath) as! ReviewListCell
         
         
-        if self.selectedCategory == "Product Quality" {
+            if self.selectedCategory == "Product Quality" {
             
             
-            switch self.positiveSegmentedController.selectedSegmentIndex {
-            case 0:
-                cell.titleText.text = self.reviews.rePosReviews[indexPath.row].summary
-                cell.reviewer.text = self.reviews.rePosReviews[indexPath.row].reviewerName
-                cell.ReviewText.text = self.reviews.rePosReviews[indexPath.row].reviewText
-            case 1:
-                cell.titleText.text = self.reviews.reNegReviews[indexPath.row].summary
-                cell.reviewer.text = self.reviews.reNegReviews[indexPath.row].reviewerName
-                cell.ReviewText.text = self.reviews.reNegReviews[indexPath.row].reviewText
-            default:
-                break
+                switch self.positiveSegmentedController.selectedSegmentIndex {
+                case 0:
+                    cell.titleText.text = self.reviews.rePosReviews[indexPath.row].summary
+                    cell.reviewer.text = self.reviews.rePosReviews[indexPath.row].reviewerName
+                    cell.ReviewText.text = self.reviews.rePosReviews[indexPath.row].reviewText
+                case 1:
+                    cell.titleText.text = self.reviews.reNegReviews[indexPath.row].summary
+                    cell.reviewer.text = self.reviews.reNegReviews[indexPath.row].reviewerName
+                    cell.ReviewText.text = self.reviews.reNegReviews[indexPath.row].reviewText
+                default:
+                    break
+                }
+            
             }
-            
-        }
-        else{
-            // None product quality
-            switch self.positiveSegmentedController.selectedSegmentIndex {
-            case 0:
-                cell.titleText.text = self.reviews.irPosReviews[indexPath.row].summary
-                cell.reviewer.text = self.reviews.irPosReviews[indexPath.row].reviewerName
-                cell.ReviewText.text = self.reviews.irPosReviews[indexPath.row].reviewText
-            case 1:
-                cell.titleText.text = self.reviews.irNegReviews[indexPath.row].summary
-                cell.reviewer.text = self.reviews.irNegReviews[indexPath.row].reviewerName
-                cell.ReviewText.text = self.reviews.irNegReviews[indexPath.row].reviewText
-            default:
-                break
+            else {
+                // None product quality
+                switch self.positiveSegmentedController.selectedSegmentIndex {
+                case 0:
+                    cell.titleText.text = self.reviews.irPosReviews[indexPath.row].summary
+                    cell.reviewer.text = self.reviews.irPosReviews[indexPath.row].reviewerName
+                    cell.ReviewText.text = self.reviews.irPosReviews[indexPath.row].reviewText
+                case 1:
+                    cell.titleText.text = self.reviews.irNegReviews[indexPath.row].summary
+                    cell.reviewer.text = self.reviews.irNegReviews[indexPath.row].reviewerName
+                    cell.ReviewText.text = self.reviews.irNegReviews[indexPath.row].reviewText
+                default:
+                    break
+                }
             }
+            return cell
         }
-        
-        
-        return cell
+        else {
+            let cell2 = tableView.dequeueReusableCell(withIdentifier: "ListSortCell", for: indexPath)
+            cell2.textLabel?.text = self.sortByTitles[indexPath.row]
+            cell2.backgroundColor = UIColor.lightGray
+            return cell2
+
+        }
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         //        if (segue.identifier == "ReviewCategory"){
         
         let DestViewController: ReviewDetailViewController = segue.destination as! ReviewDetailViewController
-        let selectedRow = tableView.indexPathForSelectedRow?.row
+        let selectedRow = reviewListTableView.indexPathForSelectedRow?.row
         DestViewController.selectedProductTitle = self.selectedProductTitle
 
         if self.selectedCategory == "Product Quality" {
@@ -166,7 +180,7 @@ class ReviewListTable: UIViewController, UITableViewDataSource, UITableViewDeleg
     func setDogImg () {
         let image = UIImage(named: "dog")
         let noDataImage = UIImageView(image: image)
-        tableView.backgroundView = noDataImage
+        reviewListTableView.backgroundView = noDataImage
     }
     
 }
