bfc26c118d1618add2ab8c32affe966ea5d821af
Team Amazon <cse498@Teams-iMac-4.local>
2017-03-23 21:55:11 -0400

    Added phrase category functionality.

---
+++ ACRA-iOS/ACRA-iOS/ACRAPhraseCategories.swift
@@ -0,0 +1,28 @@
+//
+//  ACRAPhraseCategories.swift
+//  ACRA-iOS
+//
+//  Created by Team Amazon on 3/23/17.
+//  Copyright © 2017 Team Amazon. All rights reserved.
+//
+
+import Foundation
+
+class PhraseCategories : NSObject {
+    
+    var phraseCategories: [PhraseCategory] = []
+    
+    static let sharedPhraseCategories = PhraseCategories()
+    
+    override init() {
+        super.init()
+    }
+    
+    func addPhraseCategory(category: PhraseCategory) {
+        phraseCategories.append(category)
+    }
+    
+    func clearPhraseCategories() {
+        phraseCategories.removeAll()
+    }
+}
